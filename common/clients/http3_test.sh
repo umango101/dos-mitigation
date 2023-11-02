c76faacdb5f9e8f41a7902091ee6994acdfb4f15
samd <samd@mit.edu>
2023-11-02 17:18:55 -0400

    quic setup automation

--- common/clients/http3_test.sh
+++ common/clients/http3_test.sh
@@ -8,5 +8,5 @@ server_port=8443
 remote_path="junk/1K"
 url="https://$server_ip:$server_port/$remote_path"
 
-curl -k --http3-only -s --create-dirs --no-keepalive -H 'Cache-Control: no-cache' $url -o /tmp/http_junk
+curl -k --http3-only --create-dirs --no-keepalive -H 'Cache-Control: no-cache' $url -o /tmp/http_junk
 
