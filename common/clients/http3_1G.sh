533d725dcc8187da896177191393307c2228b6d0
samd <samd@mit.edu>
2023-11-08 14:01:19 -0500

    quic experiments

---
+++ common/clients/http3_1G.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Simple HTTP Client
+
+server_ip=$1
+server_port=443
+request_interval=$2
+file_size="1G"
+
+log_dir=/tmp/logs/
+mkdir -p $log_dir
+log_file="$log_dir/http.csv"
+
+remote_path="junk/$file_size"
+url="https://$server_ip:$server_port/$remote_path"
+
+echo "status, start, end" >$log_file
+while true; do
+    start="$(date +%s%N)"
+    curl -s --http3-only -k --create-dirs --no-keepalive -H 'Cache-Control: no-cache' $url -o /tmp/http_junk
+    ok=$?
+    end="$(date +%s%N)"
+    echo "$ok,$start,$end" >>$log_file
+    sleep $request_interval
+done
+
