cf3d57c39cb67b8cbbebe3fe286ff1be023e918e
Sam DeLaughter <samdelaughter@gmail.com>
2024-01-17 15:05:22 -0500

    remove old dirs

--- common/setup/quic_server_setup.sh
+++ common/setup/quic_server_setup.sh
@@ -3,6 +3,7 @@ yes | sudo apt install git libpcre3 libpcre3-dev make python3-certbot-nginx snap
 yes | sudo apt purge nginx nginx-common nginx-core
 
 cd
+rm -rf openssl
 git clone --depth 1 -b openssl-3.1.4+quic https://github.com/quictls/openssl
 cd openssl
 mkdir build
@@ -11,9 +12,10 @@ make
 sudo make install
 
 cd ..
+rm -rf nginx-*
 wget http://hg.nginx.org/nginx/archive/tip.zip
 unzip tip.zip
-cd ~/nginx-*
+cd nginx-*
 ./auto/configure --with-debug --with-http_ssl_module --with-http_v2_module  --with-http_v3_module --with-cc-opt="-I../quictls/build/include" --with-ld-opt="-L../quictls/build/lib64"
 make
 sudo make install
