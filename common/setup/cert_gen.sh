2a1bc9aa912d94d61cb4103dda18d078761abd5d
Sam DeLaughter <samdelaughter@gmail.com>
2024-01-22 15:58:23 -0500

    separate cert_gen

---
+++ common/setup/cert_gen.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd
+sudo snap install --classic certbot
+openssl genrsa -aes256 -passout pass:gsahdg -out server.pass.key 4096
+openssl rsa -passin pass:gsahdg -in server.pass.key -out server.key
+rm server.pass.key
+# common_name=$(/usr/local/dos-mitigation/common/bin/hostname_to_ip $(hostname -s))
+openssl req -new -key server.key -out server.csr -subj /CN=${hostname -s}/
+openssl x509 -req -sha256 -days 365 -in server.csr -signkey server.key -out server.crt
+sudo mkdir -p /usr/local/nginx/certs
+sudo mv server.key /usr/local/nginx/certs/server.key
+sudo mv server.crt /usr/local/nginx/certs/server.crt
+
+sudo cp -a /usr/local/dos-mitigation/common/setup/nginx.service /lib/systemd/system/nginx.service
+sudo cp -a /usr/local/dos-mitigation/common/setup/nginx.conf /usr/local/nginx/conf/nginx.conf
+
+sudo systemctl start nginx
--- common/setup/quic_server_setup.sh
+++ common/setup/quic_server_setup.sh
@@ -11,7 +11,7 @@ mkdir build
 make
 sudo make install
 
-cd ..
+cd
 rm -rf nginx-*
 wget http://hg.nginx.org/nginx/archive/tip.zip
 unzip tip.zip
@@ -22,19 +22,19 @@ sudo make install
 sudo ldconfig ~/quictls/build/lib64
 sudo ln -s /usr/local/nginx/sbin/nginx /usr/sbin/nginx
 
-cd ..
-sudo snap install --classic certbot
-openssl genrsa -aes256 -passout pass:gsahdg -out server.pass.key 4096
-openssl rsa -passin pass:gsahdg -in server.pass.key -out server.key
-rm server.pass.key
-# common_name=$(/usr/local/dos-mitigation/common/bin/hostname_to_ip $(hostname -s))
-openssl req -new -key server.key -out server.csr -subj /CN=${hostname -s}/
-openssl x509 -req -sha256 -days 365 -in server.csr -signkey server.key -out server.crt
-sudo mkdir -p /usr/local/nginx/certs
-sudo mv server.key /usr/local/nginx/certs/server.key
-sudo mv server.crt /usr/local/nginx/certs/server.crt
+# cd
+# sudo snap install --classic certbot
+# openssl genrsa -aes256 -passout pass:gsahdg -out server.pass.key 4096
+# openssl rsa -passin pass:gsahdg -in server.pass.key -out server.key
+# rm server.pass.key
+# # common_name=$(/usr/local/dos-mitigation/common/bin/hostname_to_ip $(hostname -s))
+# openssl req -new -key server.key -out server.csr -subj /CN=${hostname -s}/
+# openssl x509 -req -sha256 -days 365 -in server.csr -signkey server.key -out server.crt
+# sudo mkdir -p /usr/local/nginx/certs
+# sudo mv server.key /usr/local/nginx/certs/server.key
+# sudo mv server.crt /usr/local/nginx/certs/server.crt
 
-sudo cp -a /usr/local/dos-mitigation/common/setup/nginx.service /lib/systemd/system/nginx.service
-sudo cp -a /usr/local/dos-mitigation/common/setup/nginx.conf /usr/local/nginx/conf/nginx.conf
+# sudo cp -a /usr/local/dos-mitigation/common/setup/nginx.service /lib/systemd/system/nginx.service
+# sudo cp -a /usr/local/dos-mitigation/common/setup/nginx.conf /usr/local/nginx/conf/nginx.conf
 
-sudo systemctl start nginx
+# sudo systemctl start nginx
