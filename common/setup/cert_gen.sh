9043382eb927c125e37e2619c57c7e556b1cbbf1
Sam DeLaughter <samdelaughter@gmail.com>
2024-01-22 16:37:06 -0500

    cert

--- common/setup/cert_gen.sh
+++ common/setup/cert_gen.sh
@@ -7,10 +7,10 @@ openssl rsa -passin pass:gsahdg -in server.pass.key -out server.key
 rm server.pass.key
 server_ip=$(/usr/local/dos-mitigation/common/bin/hostname_to_ip $(hostname -s))
 openssl req -new -key server.key -out server.csr -subj /CN=${server_ip}/
-openssl x509 -req -sha256 -days 365 -in server.csr -signkey server.key -out server.crt
+openssl x509 -req -sha256 -days 365 -in server.csr -signkey server.key -out server.pem
 sudo mkdir -p /usr/local/nginx/certs
 sudo mv server.key /usr/local/nginx/certs/server.key
-sudo mv server.crt /usr/local/nginx/certs/server.crt
+sudo mv server.pem /usr/local/nginx/certs/server.pem
 
 sudo cp -a /usr/local/dos-mitigation/common/setup/nginx.service /lib/systemd/system/nginx.service
 sudo cp -a /usr/local/dos-mitigation/common/setup/nginx.conf /usr/local/nginx/conf/nginx.conf
