610d25662d890d084f5362437691606d7944dc57
samd <samd@mit.edu>
2024-09-13 11:41:35 -0400

    continue option

--- run.py
+++ run.py
@@ -8,6 +8,10 @@ import subprocess
 import sys
 
 session = sys.argv[1]
+if len(sys.argv) > 2:
+  skip_n = int(sys.argv[2])
+else:
+  skip_n = 0
 
 code_dir = "/usr/local/dos-mitigation"
 tmp_log_path = "/tmp/ansible.log"
@@ -19,7 +23,7 @@ log_dir = "{}/logs/{}".format(code_dir, session)
 
 if os.path.exists(log_dir):
   action = input("Session with name '" + str(session) + "' already exists\n" + 
-                  "(E)xit / (o)verwrite / (i)ncrement\n"
+                  "(E)xit / (o)verwrite / (i)ncrement / (c)ontinue\n"
   )
   action = action.strip().lower()
   if action in ['o', 'overwrite']:
@@ -32,6 +36,9 @@ if os.path.exists(log_dir):
     log_dir = log_dir + "_" + str(i)
     session = session + "_" + str(i)
     os.makedirs(log_dir)
+  elif action in ['c', 'continue']:
+    existing = os.listdir(log_dir)
+    skip_n = len([i for i in existing if not i.startswith('.')])
   else:
     print('Exiting')
     sys.exit()
@@ -39,12 +46,10 @@ else:
   os.makedirs(log_dir)
 
 param_file = "{}/.parameters.json".format(log_dir)
-shutil.copyfile("{}/parameters.json".format(code_dir), param_file)
+if action not in ['c', 'continue']:
+  shutil.copyfile("{}/parameters.json".format(code_dir), param_file)
 with open(param_file) as f:
   params = json.load(f)
-
-with open('{}/mitigated_attack_types.json'.format(code_dir)) as f:
-  mitigated_attack_types = json.load(f)
 
 keys, values = zip(*params.items())
 permutations = [dict(zip(keys, v)) for v in itertools.product(*values)]
@@ -73,7 +78,13 @@ n_permutations = len(permutations)
 print("Testing {} different permutations of experiment parameters".format(n_permutations))
 i=0
 for p in permutations:
-  i += 1
+  if i < skip_n:
+    i += 1
+    print("Skipping experiment {}/{} with the following parameters:".format(i, n_permutations))
+    pprint(p)
+    continue
+  else:
+    i += 1
   timestamp = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
   print("Starting experiment {}/{} at time {} with the following parameters:".format(i, n_permutations, timestamp))
   pprint(p)
