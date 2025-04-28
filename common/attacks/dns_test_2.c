99907e244e95fa837d179d2c830c67e1af9a0013
Umang <umangbansal01@gmail.com>
2025-04-27 20:40:49 -0400

    trying to add a new dns client

--- common/attacks/dns_test_2.c
+++ common/attacks/dns_test_2.c
@@ -11,7 +11,7 @@
 
 #define MAX_PACKET_SIZE 1500
 #define DNS_PORT 53
-#define DEFAULT_SRC_IP "10.0.4.1"
+#define DEFAULT_SRC_IP "10.0.5.1"
 #define DEFAULT_DST_IP "10.0.1.1"
 
 struct pseudo_header {
---
+++ common/clients/dns_client.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Simple DNS Client
+
+server_name=$1
+server_ip=$(/usr/local/dos-mitigation/common/bin/hostname_to_ip $server_name)
+server_port=53
+request_interval=$2
+query_domain="www.google.com"
+
+log_dir=/tmp/logs/
+mkdir -p $log_dir
+log_file="$log_dir/dns.csv"
+
+# url="http://$server_ip:$server_port/junk/foo.bin"
+
+echo "status, start, end" >$log_file
+while true; do
+    start="$(date +%s%N)"
+#    curl -s --create-dirs --no-keepalive -H 'Cache-Control: no-cache' $url -o /tmp/http_junk -r 1-$file_size --cacert /usr/local/dos-mitigation/server.pem
+    dig @$server_ip $query_domain +norecurse +time=2 +tries=1 > /dev/null
+    ok=$?
+    end="$(date +%s%N)"
+    echo "$ok,$start,$end" >>$log_file
+    sleep $request_interval
+done
+
