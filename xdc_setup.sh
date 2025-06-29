c275eb395dd342e13d8a57890dc36a40bb2e1089
Umang <umangbansal01@gmail.com>
2025-06-29 14:14:54 +0000

    updated xdc_setup.sh for sphere updates

--- xdc_setup.sh
+++ xdc_setup.sh
@@ -2,6 +2,6 @@
 
 # Install Dependencies
 apt update
-apt install -y ansible nano man-db python3.10-venv zip net-tools rsync
+apt install -y ansible nano man-db python3.12-venv zip net-tools rsync unminimize
 yes | unminimize
-pip3 uninstall Jinja2
+# pip3 uninstall Jinja2
