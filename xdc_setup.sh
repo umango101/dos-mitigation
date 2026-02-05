ad0fb1ffbda51b92de3b07b9dda2e01aa7248b63
Umang <umangbansal01@gmail.com>
2026-02-05 23:08:13 +0000

    experiment code retooling

--- xdc_setup.sh
+++ xdc_setup.sh
@@ -2,6 +2,6 @@
 
 # Install Dependencies
 apt update
-apt install -y ansible nano man-db python3.12-venv zip net-tools rsync unminimize
-yes | unminimize
+apt install -y ansible nano man-db python3-venv zip net-tools rsync
+# yes | unminimize
 # pip3 uninstall Jinja2
